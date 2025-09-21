a6df17816a073fe4ec9cb7d1c09cba9dce35d27b
Fatima <jazaeifateme@gmail.com>
2025-09-21 21:52:57 +0330

    Add Entities (User, Contact)

---
+++ PhoneBook.Domain/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
